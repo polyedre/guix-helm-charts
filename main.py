a1ff1d68507f9a9f2a60b8d1943c6b3880ff733e
Polyedre <polyedre@disroot.org>
2024-04-04 19:28:06 +0200

    Extract all Helm repositories from Artifact Hub

--- main.py
+++ main.py
@@ -26,12 +26,6 @@ import yaml
 import jinja2
 import requests
 
-repositories = {
-    "ilum": "https://charts.ilum.cloud",
-    "bitnami": "https://charts.bitnami.com/bitnami",
-    "artifact-hub": "https://artifacthub.github.io/helm-charts/"
-}
-
 HEADER = """
 (define-module (helm {{ repo_name }} {{ chart_name }})
   #:use-module (guix utils)
@@ -62,31 +56,78 @@ CHART_DEFINITION = """
    (license #f)))
 """
 
-try:
-    os.mkdir("helm")
-except FileExistsError:
-    pass
+def get_repositories():
+    batch_size = 60  # max supported by artifacthub
 
-for repo_name, repo_url in repositories.items():
+    repositories = {}
+
+    offset = 0
+    while True:
+        r = requests.get(f"https://artifacthub.io/api/v1/repositories/search?offset={offset}&limit={batch_size}&kind=0")
+        data = r.json()
+
+        if not data:
+            break
+
+        for entry in data:
+            repositories[entry["name"]] = entry["url"]
+
+        offset += batch_size
+
+    print(f"Loaded {len(repositories)} repositories")
+
+    return repositories
+
+if __name__ == '__main__':
+    # repositories = get_repositories()
+
+    # with open("repositories.yml", "w") as repo_config:
+    #     repo_config.write("---\n")
+    #     repo_config.write("repositories:\n")
+    #     for key, value in repositories.items():
+    #         repo_config.write(f"  {key}: \"{value}\"\n")
+
+    with open("repositories.yml", "r") as repo_config:
+        repositories = yaml.safe_load(repo_config)["repositories"]
+
     try:
-        os.mkdir(f"helm/{repo_name}")
+        os.mkdir("helm")
     except FileExistsError:
         pass
 
-    charts = yaml.safe_load(requests.get(f"{repo_url}/index.yaml").text)
+    for repo_name, repo_url in repositories.items():
+        try:
+            os.mkdir(f"helm/{repo_name}")
+        except FileExistsError:
+            pass
 
-    for chart_name, entries in charts["entries"].items():
-        with open(f"helm/{repo_name}/{chart_name}.scm", "w") as module:
-            module.write(jinja2.Template(HEADER).render(repo_name=repo_name, chart_name=chart_name))
-            for entry in entries:
-                url: str = entry["urls"][0]
-                if not url.startswith("http"):
-                    url = f"{repo_url}/{url}"
-                module.write(jinja2.Template(CHART_DEFINITION).render(
-                    name=chart_name,
-                    version=entry["version"],
-                    url=url,
-                    home=entry.get("home", ""),
-                    description=entry.get("description", ""),
-                    chart_url=url
-                ))
+        r = None
+        try:
+            r = requests.get(f"{repo_url}/index.yaml")
+            if r.status_code != 200:
+                print(f"Status code is {r.status_code}!")
+                continue
+            charts = yaml.safe_load(r.text)
+        except Exception as exception:
+            print(f"Error with {repo_url}/index.yaml: {exception}")
+            continue
+
+        if not "entries" in charts:
+            print(charts)
+            continue
+
+        for chart_name, entries in charts["entries"].items():
+            with open(f"helm/{repo_name}/{chart_name}.scm", "w") as module:
+                module.write(jinja2.Template(HEADER).render(repo_name=repo_name, chart_name=chart_name))
+                for entry in entries:
+                    url: str = entry["urls"][0]
+                    if not url.startswith("http"):
+                        url = f"{repo_url}/{url}"
+                    module.write(jinja2.Template(CHART_DEFINITION).render(
+                        name=chart_name,
+                        version=entry["version"],
+                        url=url,
+                        home=entry.get("home", ""),
+                        description=entry.get("description", ""),
+                        chart_url=url
+                    ))
